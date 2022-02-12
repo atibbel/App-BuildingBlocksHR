de8563c8dfca7d85e701b2687afceaa469a396c6
amangill13 <kaur0628@algonquinlive.com>
2022-02-12 15:37:43 -0500

    Made popout menus and main activities according to the three plans provided

---
+++ app/src/main/java/algonquin/cst2335/buildingblockshr/LoginActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.buildingblockshr;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
